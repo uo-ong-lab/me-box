277b27b72adce617e334afd62c5e51b1315f44e8
theymightbepotatoesafter <theymightbepotatoesafter@users.noreply.github.com>
2020-01-27 15:24:29 -0800

    v1.3
    Have everything working except the UART to send the pulse counts. The UART is connecting, it's just not sending any data just yet

--- blinky.c
+++ blinky.c
@@ -27,6 +27,9 @@
 #include "driverlib/ssi.h"
 #include "driverlib/systick.h"
 #include "driverlib/ssi.c"
+#include "driverlib/uart.h"
+#include "driverlib/fpu.h"
+#include "driverlib/adc.h"
 
 #ifdef DEBUG
 void
@@ -36,8 +39,6 @@ __error__(char *pcFilename, uint32_t ui32Line)
 }
 #endif
 
-#define NUM_SSI_DATA 8
-
 uint32_t STEP_SIZE = 1000;
 uint32_t low_bound = 10000;
 uint32_t high_bound = 2000000;
@@ -45,22 +46,38 @@ uint32_t f_reg;
 uint32_t i;
 uint32_t frequencyRegister[2][1000];
 uint32_t rows;
-uint32_t ADCValue;
 uint32_t lowVoltage;
 uint32_t midVoltage;
 uint32_t highVoltage;
+uint32_t ADCCutoff[1] = {0};
+uint32_t voltage1 = 0;
+uint32_t voltage2 = 1;
+uint32_t voltage3 = 3;
+uint32_t voltage4 = 4;
+uint32_t refVoltage = 5;
+uint32_t voltCode[4];
+
+//The SSI communication code was grabbed from an engineering project from Purdue. The link to the paper is
+//https://engineering.purdue.edu/ece477/Archive/2014/Spring/S14-Grp1/docs/software/LM4F-LaunchPad-11%20-%20SPI.pdf
+//The bitwise reverse function was taken out because it is not needed.
 
 void
-PeriphEnable(){
+PeriphEnable(void){
     SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI1);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+    SysCtlPeripheralReset(SYSCTL_PERIPH_ADC0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
 }
 
 void
-PinConfig(){
+PinConfig(void){
+    GPIOPinTypeADC(GPIO_PORTD_BASE, GPIO_PIN_2);
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
     GPIOPinConfigure(GPIO_PA2_SSI0CLK);
     GPIOPinConfigure(GPIO_PA3_SSI0FSS);
     GPIOPinConfigure(GPIO_PA5_SSI0TX);
@@ -72,16 +89,55 @@ PinConfig(){
     GPIOPinTypeGPIOInput(GPIO_PORTC_BASE, GPIO_PIN_6);
     GPIOPinTypeSSI(GPIO_PORTA_BASE, GPIO_PIN_5|GPIO_PIN_3|GPIO_PIN_2);
     GPIOPinTypeSSI(GPIO_PORTD_BASE, GPIO_PIN_3|GPIO_PIN_1|GPIO_PIN_0);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
 }
 
 void
-DataSend(uint32_t data1, uint32_t data2, uint32_t data3){
+UARTIntHandler(void){
+    uint32_t ui32Status;
+    ui32Status = UARTIntStatus(UART0_BASE, true);
+    UARTIntClear(UART0_BASE, ui32Status);
+}
+
+void
+UARTInit(void){
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+    IntEnable(INT_UART0);
+    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+    IntMasterEnable();
+}
+
+void
+SSIInit(void){
+    SSIConfigSetExpClk(SSI0_BASE,SysCtlClockGet(),SSI_FRF_MOTO_MODE_0,SSI_MODE_MASTER,10000,16);
+    SSIConfigSetExpClk(SSI1_BASE,SysCtlClockGet(),SSI_FRF_MOTO_MODE_0,SSI_MODE_MASTER,10000,11);
+    SSIEnable(SSI0_BASE);
+    SSIEnable(SSI1_BASE);
+}
+
+void
+DataSend(uint32_t data1, uint32_t data2, uint32_t data3, uint32_t ui32Count){
     //sends the pulse numbers from the pulse counter, through UART, to the computer
+    //while(ui32Count--){
+    //    UARTCharPutNonBlocking(UART0_BASE, (uint32_t)"yeet");
+    //}
+    while(ui32Count--){
+        // Write the next character to the UART.
+        UARTCharPutNonBlocking(UART0_BASE, data1);
+    }
+    while(ui32Count--){
+        // Write the next character to the UART.
+        UARTCharPutNonBlocking(UART0_BASE, data2);
+    }
+    while(ui32Count--){
+        // Write the next character to the UART.
+        UARTCharPutNonBlocking(UART0_BASE, data3);
+    }
 }
 
 void
-FreqRegBits(){
+FreqRegBits(void){
     uint32_t rows = (int)((high_bound - low_bound)/STEP_SIZE);
     uint32_t reg;
     for(i = 0; i < rows; i++){
@@ -93,25 +149,49 @@ FreqRegBits(){
 
 }
 
-int
-ADCReader(){
-    //reads the "peak" value from the signal response to determine when to cut off the pulse counting
-    uint32_t ammount; //the cut off value
-    uint32_t value; //the value given by the ADC
-    if(value > ammount){
-        return 0;
-    }
-    else if(value <= ammount){
-        return 1;
-    }
+void
+ADCInit(void){
+    ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, (ADC_CTL_CH6 | ADC_CTL_IE | ADC_CTL_END));
+    ADCSequenceEnable(ADC0_BASE, 3);
+    ADCIntClear(ADC0_BASE, 3);
 }
 
 int
-PulseCounter(){
-    highVoltage = 0;
-    midVoltage = 0;
-    lowVoltage = 0;
-    while(ADCReader() == 0){
+ADCValue(void){
+    i = 0;
+    while(i != 1)
+        {
+            ADCProcessorTrigger(ADC0_BASE, 3);
+            while(!ADCIntStatus(ADC0_BASE, 3, false)){}
+            ADCIntClear(ADC0_BASE, 3);
+            ADCSequenceDataGet(ADC0_BASE, 3, ADCCutoff);
+            i = 1;
+        }
+    return ADCCutoff[0];
+}
+
+int
+ADCCutoffReader(void){
+    //reads the "peak" value from the signal response to determine when to cut off the pulse counting
+    uint32_t cutoffVoltage = 0.3; //the cut off value
+    uint32_t ADCRipplevalue = ADCValue();//the value given by the ADC
+    uint32_t returnValue;
+    if(ADCRipplevalue > cutoffVoltage){
+        returnValue = 0;
+    }
+    else if(ADCRipplevalue <= cutoffVoltage){
+        returnValue = 1;
+    }
+    return returnValue;
+}
+
+int
+PulseCounter(void){
+    highVoltage = 4.5;
+    midVoltage = 2.5;
+    lowVoltage = 1;
+    while(ADCCutoffReader() == 0){
         uint32_t low = GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_4);
         uint32_t mid = GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_5);
         uint32_t high = GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_6);
@@ -125,12 +205,12 @@ PulseCounter(){
             lowVoltage += 1;
         }
     }
-    DataSend(highVoltage, midVoltage, lowVoltage);
+    DataSend(highVoltage, midVoltage, lowVoltage, 16);
     return 1;
 }
 
 void
-SineWavePulse(){
+SineWavePulse(void){
     uint32_t rows = (int)((high_bound - low_bound)/STEP_SIZE);
     //highest frequency that can be in the first 14 bits of the frequency register is 976.5 Hz which is below the predetermined start
     //frequency of 1000 Hz. The lower 14 bits will still be programmed in but there will be logic to determine if they're needed or not.
@@ -156,13 +236,6 @@ SineWavePulse(){
     }
 }
 
-uint32_t voltage1 = 0;
-uint32_t voltage2 = 1;
-uint32_t voltage3 = 3;
-uint32_t voltage4 = 4;
-uint32_t refVoltage = 5;
-uint32_t voltCode[4];
-
 void
 voltCodeGenerator(uint32_t voltage1, uint32_t voltage2, uint32_t voltage3, uint32_t voltage4, uint32_t refVoltage){
 
@@ -174,11 +247,11 @@ voltCodeGenerator(uint32_t voltage1, uint32_t voltage2, uint32_t voltage3, uint3
 }
 
 void
-DCVoltage(){
-    uint32_t location1 = (int)(pow(2, 0));
-    uint32_t location2 = (int)(pow(2, 9));
-    uint32_t location3 = (int)(pow(2, 10));
-    uint32_t location4 = (int)(pow(2, 9) + pow(2, 10));
+DCVoltage(void){
+    uint32_t location1 = 0;
+    uint32_t location2 = (0b01000000000);
+    uint32_t location3 = (0b10000000000);
+    uint32_t location4 = (0b11000000000);
     uint32_t voltbits1 = voltCode[0] + location1;
     uint32_t voltbits2 = voltCode[1] + location2;
     uint32_t voltbits3 = voltCode[2] + location3;
@@ -197,20 +270,14 @@ DCVoltage(){
     GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_6, 0b1);
 }
 
-//The SSI communication code was grabbed from an engineering project from Purdue. The link to the paper is
-//https://engineering.purdue.edu/ece477/Archive/2014/Spring/S14-Grp1/docs/software/LM4F-LaunchPad-11%20-%20SPI.pdf
-//The bitwise reverse function was taken out because it is not needed.
-
 int main(void){
-
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
     PeriphEnable();
     PinConfig();
+    ADCInit();
+    UARTInit();
+    SSIInit();
     FreqRegBits();
-    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
-    SSIConfigSetExpClk(SSI0_BASE,SysCtlClockGet(),SSI_FRF_MOTO_MODE_0,SSI_MODE_MASTER,10000,16);
-    SSIConfigSetExpClk(SSI1_BASE,SysCtlClockGet(),SSI_FRF_MOTO_MODE_0,SSI_MODE_MASTER,10000,11);
-    SSIEnable(SSI0_BASE);
-    SSIEnable(SSI1_BASE);
     voltCodeGenerator(voltage1, voltage2, voltage3, voltage4, refVoltage);
     DCVoltage();
     SysCtlDelay(100000);
